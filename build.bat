47abcb53e15f7875bf3e14bc4cd2e784167c44ab
KurkG4 <shaggydogg6969@gmail.com>
2020-07-31 13:18:48 -0400

    build attempt 1

--- build.bat
+++ build.bat
@@ -1,6 +1,17 @@
 @echo off
+echo Build started...
+asm68k /o ae- /p Sonic2Cl.asm,s2built.bin
 
-IF EXIST s2built.bin move /Y s2built.bin s2built.prev.bin >NUL
-Build_Source\asm68k  /k /p /o ae- Sonic2CL.asm, s2built.bin >errors.txt, ,Sonic2CL.lst
-Build_Source\fixheadr s2built.bin
+IF NOT EXIST s2built.bin goto LABLERR
+rem rompad s2built.bin 255 0
+rem fixheadr.exe s2built.bin
+
+echo Build succeeded
+
+goto LABLDONE
+
+:LABLERR
+echo Build failed
+pause
+:LABLDONE
 pause
